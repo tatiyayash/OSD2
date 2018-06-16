8cf46dec66b610db92978a004f2ecd245052305a
tatiyayash <40317843+tatiyayash@users.noreply.github.com>
2018-06-16 20:54:48 +0800

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-function openmypage() {
-    console.log("injecting");
-    browser.tabs.create({
-        "url": "index.html"
-    });
-}
-
-browser.browserAction.onClicked.addListener(openmypage);
